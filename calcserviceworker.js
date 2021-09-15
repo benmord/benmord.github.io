4f8fa9894c29f664fc8d4f110ce4645fcdc9b377
Benjamin Mord <ben@mord.io>
2021-09-15 17:44:09 -0400

    trying to make webapp responsive while offline

--- calcserviceworker.js
+++ calcserviceworker.js
@@ -2,15 +2,15 @@ self.addEventListener('install', function(event) {
  event.waitUntil(
   caches.open('calc-static-v1').then(function (cache) {
    return cache.addAll([
-    'calc.html',
-    'icons/arsia_icon_72x72.png',
-    'icons/arsia_icon_96x96.png',
-    'icons/arsia_icon_128x128.png',
-    'icons/arsia_icon_144x144.png',
-    'icons/arsia_icon_152x152.png',
-    'icons/arsia_icon_192x192.png',
-    'icons/arsia_icon_384x384.png',
-    'icons/arsia_icon_512x512.png'
+    'https://benmord.github.io/calc.html',
+    'https://benmord.github.io/icons/arsia_icon_72x72.png',
+    'https://benmord.github.io/icons/arsia_icon_96x96.png',
+    'https://benmord.github.io/icons/arsia_icon_128x128.png',
+    'https://benmord.github.io/icons/arsia_icon_144x144.png',
+    'https://benmord.github.io/icons/arsia_icon_152x152.png',
+    'https://benmord.github.io/icons/arsia_icon_192x192.png',
+    'https://benmord.github.io/icons/arsia_icon_384x384.png',
+    'https://benmord.github.io/icons/arsia_icon_512x512.png'
    ]);
   }),
  );
