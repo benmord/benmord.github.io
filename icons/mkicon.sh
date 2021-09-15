a197bc4ee5f6d6fbcb3c10e115a194ff8f3897cc
Benjamin Mord <ben@mord.io>
2021-09-15 08:44:33 -0400

    added manifest and icons

---
+++ icons/mkicon.sh
@@ -0,0 +1,9 @@
+convert arsia_logo.png -resize 512x512 arsia_icon_512x512.png
+convert arsia_logo.png -resize 384x384 arsia_icon_384x384.png
+convert arsia_logo.png -resize 192x192 arsia_icon_192x192.png
+convert arsia_logo.png -resize 152x152 arsia_icon_152x152.png
+convert arsia_logo.png -resize 144x144 arsia_icon_144x144.png
+convert arsia_logo.png -resize 128x128 arsia_icon_128x128.png
+convert arsia_logo.png -resize 96x96 arsia_icon_96x96.png
+convert arsia_logo.png -resize 72x72 arsia_icon_72x72.png
+
